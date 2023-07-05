4a21dbd432186bb2de33d703a8f37cd39a0d7b5e
ddussi <tmqdyfud1@gmail.com>
2023-07-06 00:54:19 +0900

    23/07/06

---
+++ baekjoon/dynamic_/1463 1로 만들기.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(/\s/).map(Number);
+function answer(n){
+    let dp = new Array(n + 1).fill(0);
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + 1; // 1을 빼는 경우
+
+    if (i % 2 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 2로 나누는 경우
+    }
+
+    if (i % 3 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 3로 나누는 경우
+    }
+  }
+
+  return dp[n];
+}
+console.log(answer(input[0]));
